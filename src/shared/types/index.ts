78744ad652a1b49576139601f87706b7ebb61a5f
kirill korolev <kkirillcore@gmail.com>
2025-02-23 15:57:05 +0100

    feat: add DI

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export { Image } from './image.type.js';
 export { Offer } from './offer.type.js';
 export { User } from './user.type.js';
 export { MockServerData } from './mock-server-data.type.js';
+export { Component } from './component.enum.js';
